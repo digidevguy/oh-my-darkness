78bd5122a5e0b426088aa661c49f88a6543e0989
Permafun <49506518+Permafun@users.noreply.github.com>
2022-04-20 17:18:53 -0500

    Refactored roster to iterable

--- pages/roster.js
+++ pages/roster.js
@@ -1,6 +1,7 @@
 import { Flex } from '@chakra-ui/react';
 import Banner from '../components/Banner';
 import MemberProfile from '../components/MemberProfile';
+import memberInfo from '../lib/memberInfo';
 
 const RosterPage = () => {
 	return (
@@ -12,75 +13,20 @@ const RosterPage = () => {
 				title='Meet the Assholes'
 			/>
 			<Flex flexDirection='column' maxW='1200px' m='auto'>
-				<MemberProfile
-					name='Jet Laudir'
-					link='https://na.finalfantasyxiv.com/lodestone/character/3640299/'
-					nickname='Psuedo FC Master'
-					jobs='Stripper'
-					reasonToPlay="I know I'm the leader, but..."
-					interests='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius
-                    quam quisque id diam vel.'
-					src='/images/members/dbs8uxu-3febdb99-4532-4d3d-8dd0-2eeec3e6dda5.png'
-					width={653}
-					height={784}
-					orientation='row'
-				/>
-				<MemberProfile
-					name='Bardon Ambire'
-					link='https://na.finalfantasyxiv.com/lodestone/character/3640299/'
-					nickname='Squawker'
-					jobs='MNK, SMN'
-					reasonToPlay='Oh my lord...'
-					interests='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius
-                    quam quisque id diam vel.'
-					src='/images/members/triangle-2020-06-04T170933.webp'
-					width={2391}
-					height={2700}
-					orientation='row-reverse'
-				/>
-				<MemberProfile
-					name='Ryohou Kamina'
-					link='https://na.finalfantasyxiv.com/lodestone/character/3249244/'
-					nickname='"Worst Sage in Coeurl"'
-					jobs='SGE, PLD'
-					reasonToPlay="I've been playing XIV since 1.0, and the community and story have
-                    kept me going the entire time!"
-					interests='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius
-                    quam quisque id diam vel.'
-					src='/images/members/oath_by_pizza_surgeon_d9m166u-edited.jpg'
-					width={891}
-					height={1080}
-				/>
-				<MemberProfile
-					name='Byron Endover'
-					link='https://na.finalfantasyxiv.com/lodestone/character/25852892/'
-					nickname='.........'
-					jobs='SAM,IDIOT'
-					reasonToPlay='#horny'
-					interests='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius
-                    quam quisque id diam vel.'
-					src='/images/members/PatrickStar_467pin.webp'
-					width={1000}
-					height={1750}
-					orientation='row-reverse'
-				/>
-				<MemberProfile
-					name='Soleil Courtois'
-					link='https://na.finalfantasyxiv.com/lodestone/character/16806074/'
-					nickname='Frenchie'
-					jobs='WHM,AST,SAM'
-					reasonToPlay="I can't resist playing a catboi!"
-					interests='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius
-                    quam quisque id diam vel.'
-					src='/images/members/sample-873066de9f512f102e37b0a43019ff6e.jpg'
-					width={850}
-					height={1441}
-				/>
+				{memberInfo.map((member, i) => (
+					<MemberProfile
+						name={member.name}
+						link={member.link}
+						nickname={member.nickname}
+						jobs={member.jobs}
+						reasonToPlay={member.reasonToPlay}
+						interests={member.interests}
+						src={member.src}
+						width={member.width}
+						height={member.height}
+						orientation={member.orientation}
+					/>
+				))}
 			</Flex>
 		</Flex>
 	);
