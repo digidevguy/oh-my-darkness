2d5af14516acc075a8cce2be43371d63277fd26f
Permafun <49506518+Permafun@users.noreply.github.com>
2022-06-29 17:37:22 -0500

    Adding new content for release

--- pages/about.js
+++ pages/about.js
@@ -22,115 +22,29 @@ const AboutPage = () => {
 			/>
 			<Flex flexDirection='column' gap='1rem' p={5}>
 				<Heading>How We Started</Heading>
-				<Text>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum
-					nibh tellus molestie nunc non blandit massa. Convallis tellus id
-					interdum velit laoreet id. Tortor pretium viverra suspendisse potenti
-					nullam ac tortor vitae. Diam quam nulla porttitor massa id neque. Dui
-					ut ornare lectus sit amet est placerat. Gravida in fermentum et
-					sollicitudin. Ante metus dictum at tempor commodo ullamcorper a lacus.
-					Iaculis nunc sed augue lacus viverra vitae congue. Eget felis eget
-					nunc lobortis mattis. Id neque aliquam vestibulum morbi blandit cursus
-					risus at ultrices.
-				</Text>
-				<Text>
-					Lectus proin nibh nisl condimentum id venenatis a condimentum vitae.
-					Egestas purus viverra accumsan in nisl. Quam quisque id diam vel quam
-					elementum pulvinar etiam non. Cursus metus aliquam eleifend mi in
-					nulla posuere. Tincidunt augue interdum velit euismod in pellentesque.
-					At ultrices mi tempus imperdiet nulla malesuada pellentesque. Odio
-					aenean sed adipiscing diam. Lectus mauris ultrices eros in cursus
-					turpis massa tincidunt. Eu facilisis sed odio morbi quis commodo odio.
-					At tempor commodo ullamcorper a lacus vestibulum sed arcu non.
-					Elementum facilisis leo vel fringilla est ullamcorper eget. Nibh nisl
-					condimentum id venenatis a condimentum. Massa tempor nec feugiat nisl.
-					Lacus sed viverra tellus in hac habitasse platea dictumst vestibulum.
-					Neque volutpat ac tincidunt vitae semper quis. Tellus cras adipiscing
-					enim eu turpis egestas pretium.
-				</Text>
-				<Text>
-					Morbi tristique senectus et netus et malesuada fames. Neque viverra
-					justo nec ultrices dui sapien eget mi proin. A erat nam at lectus urna
-					duis convallis convallis tellus. Risus quis varius quam quisque id
-					diam vel. Laoreet non curabitur gravida arcu ac tortor. Quis viverra
-					nibh cras pulvinar mattis nunc sed blandit. Mollis nunc sed id semper
-					risus. Est placerat in egestas erat imperdiet sed euismod nisi porta.
-					Quis enim lobortis scelerisque fermentum dui faucibus in ornare quam.
-					Sed blandit libero volutpat sed.
-				</Text>
+				<Text>Leadership bad, started our own shiz</Text>
+
 				<Heading>Our Community Goals</Heading>
-				<Text>
-					Lectus proin nibh nisl condimentum id venenatis a condimentum vitae.
-					Egestas purus viverra accumsan in nisl. Quam quisque id diam vel quam
-					elementum pulvinar etiam non. Cursus metus aliquam eleifend mi in
-					nulla posuere. Tincidunt augue interdum velit euismod in pellentesque.
-					At ultrices mi tempus imperdiet nulla malesuada pellentesque. Odio
-					aenean sed adipiscing diam. Lectus mauris ultrices eros in cursus
-					turpis massa tincidunt. Eu facilisis sed odio morbi quis commodo odio.
-					At tempor commodo ullamcorper a lacus vestibulum sed arcu non.
-					Elementum facilisis leo vel fringilla est ullamcorper eget. Nibh nisl
-					condimentum id venenatis a condimentum. Massa tempor nec feugiat nisl.
-					Lacus sed viverra tellus in hac habitasse platea dictumst vestibulum.
-					Neque volutpat ac tincidunt vitae semper quis. Tellus cras adipiscing
-					enim eu turpis egestas pretium.
-				</Text>
+				<Text>Building a community with a focus on raiding.</Text>
 				<OrderedList>
 					<ListItem>Suck</ListItem>
 					<ListItem>meh</ListItem>
 					<ListItem>Thumb?</ListItem>
 				</OrderedList>
 				<Text>
-					Morbi tristique senectus et netus et malesuada fames. Neque viverra
-					justo nec ultrices dui sapien eget mi proin. A erat nam at lectus urna
-					duis convallis convallis tellus. Risus quis varius quam quisque id
-					diam vel. Laoreet non curabitur gravida arcu ac tortor. Quis viverra
-					nibh cras pulvinar mattis nunc sed blandit. Mollis nunc sed id semper
-					risus. Est placerat in egestas erat imperdiet sed euismod nisi porta.
-					Quis enim lobortis scelerisque fermentum dui faucibus in ornare quam.
-					Sed blandit libero volutpat sed.
+					Building an open and welcoming community that accepts all types of ppl
+					and dicks.
 				</Text>
 				<Heading>Our Members and Peak Times</Heading>
 				<Text>
-					Lectus proin nibh nisl condimentum id venenatis a condimentum vitae.
-					Egestas purus viverra accumsan in nisl. Quam quisque id diam vel quam
-					elementum pulvinar etiam non. Cursus metus aliquam eleifend mi in
-					nulla posuere. Tincidunt augue interdum velit euismod in pellentesque.
-					At ultrices mi tempus imperdiet nulla malesuada pellentesque. Odio
-					aenean sed adipiscing diam. Lectus mauris ultrices eros in cursus
-					turpis massa tincidunt. Eu facilisis sed odio morbi quis commodo odio.
-					At tempor commodo ullamcorper a lacus vestibulum sed arcu non.
-					Elementum facilisis leo vel fringilla est ullamcorper eget. Nibh nisl
-					condimentum id venenatis a condimentum. Massa tempor nec feugiat nisl.
-					Lacus sed viverra tellus in hac habitasse platea dictumst vestibulum.
-					Neque volutpat ac tincidunt vitae semper quis. Tellus cras adipiscing
-					enim eu turpis egestas pretium.
-				</Text>
-				<Text>
-					Morbi tristique senectus et netus et malesuada fames. Neque viverra
-					justo nec ultrices dui sapien eget mi proin. A erat nam at lectus urna
-					duis convallis convallis tellus. Risus quis varius quam quisque id
-					diam vel. Laoreet non curabitur gravida arcu ac tortor. Quis viverra
-					nibh cras pulvinar mattis nunc sed blandit. Mollis nunc sed id semper
-					risus. Est placerat in egestas erat imperdiet sed euismod nisi porta.
-					Quis enim lobortis scelerisque fermentum dui faucibus in ornare quam.
-					Sed blandit libero volutpat sed.
+					The majority of our group is based on the east coast, and generally
+					active in the evenings during the week and various times during the
+					weekend.
 				</Text>
 				<Heading>Weekly and Seasonal Events</Heading>
 				<Text>
-					Lectus proin nibh nisl condimentum id venenatis a condimentum vitae.
-					Egestas purus viverra accumsan in nisl. Quam quisque id diam vel quam
-					elementum pulvinar etiam non. Cursus metus aliquam eleifend mi in
-					nulla posuere. Tincidunt augue interdum velit euismod in pellentesque.
-					At ultrices mi tempus imperdiet nulla malesuada pellentesque. Odio
-					aenean sed adipiscing diam. Lectus mauris ultrices eros in cursus
-					turpis massa tincidunt. Eu facilisis sed odio morbi quis commodo odio.
-					At tempor commodo ullamcorper a lacus vestibulum sed arcu non.
-					Elementum facilisis leo vel fringilla est ullamcorper eget. Nibh nisl
-					condimentum id venenatis a condimentum. Massa tempor nec feugiat nisl.
-					Lacus sed viverra tellus in hac habitasse platea dictumst vestibulum.
-					Neque volutpat ac tincidunt vitae semper quis. Tellus cras adipiscing
-					enim eu turpis egestas pretium.
+					As our community grows we are interested in adding more activities as
+					a group. Currently maps and raid groups.
 				</Text>
 				<UnorderedList>
 					<ListItem>One</ListItem>
@@ -140,16 +54,8 @@ const AboutPage = () => {
 				</UnorderedList>
 				<Heading>Housing</Heading>
 				<Text>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum
-					nibh tellus molestie nunc non blandit massa. Convallis tellus id
-					interdum velit laoreet id. Tortor pretium viverra suspendisse potenti
-					nullam ac tortor vitae. Diam quam nulla porttitor massa id neque. Dui
-					ut ornare lectus sit amet est placerat. Gravida in fermentum et
-					sollicitudin. Ante metus dictum at tempor commodo ullamcorper a lacus.
-					Iaculis nunc sed augue lacus viverra vitae congue. Eget felis eget
-					nunc lobortis mattis. Id neque aliquam vestibulum morbi blandit cursus
-					risus at ultrices.
+					We recently moved to a larger FC house in the Kugane area. Ask
+					Frenchie.
 				</Text>
 				<UnorderedList>
 					<ListItem>One</ListItem>
@@ -159,40 +65,42 @@ const AboutPage = () => {
 				</UnorderedList>
 				<Heading>FC Chest</Heading>
 				<Text>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum
-					nibh tellus molestie nunc non blandit massa. Convallis tellus id
-					interdum velit laoreet id. Tortor pretium viverra suspendisse potenti
-					nullam ac tortor vitae. Diam quam nulla porttitor massa id neque. Dui
-					ut ornare lectus sit amet est placerat. Gravida in fermentum et
-					sollicitudin. Ante metus dictum at tempor commodo ullamcorper a lacus.
-					Iaculis nunc sed augue lacus viverra vitae congue. Eget felis eget
-					nunc lobortis mattis. Id neque aliquam vestibulum morbi blandit cursus
-					risus at ultrices.
+					As a general rule the chest is open to other players as long as
+					everyone contributes to replenishing what is there. We have several
+					crafters in our group currently so crystals and crafting materials are
+					considered at a premium: generally you can use what is there but
+					please be considerate of the other member's needs.
 				</Text>
+				<Text></Text>
 				<UnorderedList>
-					<ListItem>One</ListItem>
-					<ListItem>Two</ListItem>
-					<ListItem>Three</ListItem>
-					<ListItem>Four</ListItem>
+					<ListItem>
+						For consummable usage, please ask an officer before withdrawing
+						large stacks.
+					</ListItem>
+					<ListItem>Gil withdrawals are restricted to officer use.</ListItem>
+					<ListItem>
+						Restricted items in later tabs are available with an officer's
+						approval. Please communicate this in the Discord server or in FC
+						chat.
+					</ListItem>
 				</UnorderedList>
 				<Heading>Raiding</Heading>
 				<Text>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum
-					nibh tellus molestie nunc non blandit massa. Convallis tellus id
-					interdum velit laoreet id. Tortor pretium viverra suspendisse potenti
-					nullam ac tortor vitae. Diam quam nulla porttitor massa id neque. Dui
-					ut ornare lectus sit amet est placerat. Gravida in fermentum et
-					sollicitudin. Ante metus dictum at tempor commodo ullamcorper a lacus.
-					Iaculis nunc sed augue lacus viverra vitae congue. Eget felis eget
-					nunc lobortis mattis. Id neque aliquam vestibulum morbi blandit cursus
-					risus at ultrices.
+					We have one static raid group and working on expanding with more
+					members for both subtitutions and additional raid groups. Common
+					courtesy rules apply when discussing raid topics and during events: we
+					make sure to have fun while raiding but maintain focus on making
+					progression.
 				</Text>
 				<UnorderedList>
-					<ListItem>One</ListItem>
-					<ListItem>Two</ListItem>
-					<ListItem>Three</ListItem>
+					<ListItem>Raid nights are at least twice a week.</ListItem>
+					<ListItem>
+						Subtitute players are recommended to be able to flex within two
+						different raid roles (ex. healer, tank, ranged DPS, etc.)
+					</ListItem>
+					<ListItem>
+						Streaming terms of service rules apply during raid times.
+					</ListItem>
 					<ListItem>Four</ListItem>
 				</UnorderedList>
 				<Text>
